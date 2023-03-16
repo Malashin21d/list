d9cebea043f3c60f22729679f84d6a72df068491
Сергей <@ANDREYNOTEBOOK>
2023-03-17 00:03:06 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿
+        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+    {
+        class Guns
+        {
+            List<string> guns = new List<string>();
+
+
+            public void Add(string a)
+            {
+                guns.Add(a);
+
+            }
+            public void Remove(string p)
+            {
+                guns.Remove(p);
+                Console.WriteLine("~Deleted");
+            }
+            public string GetById(int id)
+            {
+                return "Элемент с айди " + id + " " + guns[id];
+            }
+            public void Insert(int id, string p)
+            {
+                guns.Insert(id, p);
+
+            }
+            public void Update(int id, string p)
+            {
+                guns.RemoveRange(id, 1);
+                guns.Insert(id, p);
+            }
+            public string FindName(int p, int condition)
+            {
+                if (guns.Exists(p => p.Length == condition))// проверяю есть ли элемент с заданным условием длинны
+                {
+                    return guns.Find(p => p.Length == condition);
+                }
+                else
+                {
+                    return "Такого оружия нет в списке";
+                }
+
+            }
+            public void GetAll()
+            {
+                foreach (string p in guns)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+    }
+
+
---
+++ GunInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class GunInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public GunInfo(int id, string name, int price)
+        {
+            Id = id;
+            Name = name;
+
+            Price = Price;
+        }
+        public override string ToString()
+        {
+            return String.Format("Id {0)" +
+                "Name {1}" +
+                "Age {2}", Id, Name, Price);
+        }
+    }
+}
